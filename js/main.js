e20a03489e10e003cc33c4290ab439aa5d87db0b
Thais Milena <91845071+thaismilenam@users.noreply.github.com>
2022-10-26 22:00:16 -0300

    errado, refazer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 const controle = document.querySelectorAll("[data-controle]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
@@ -16,5 +49,21 @@ function manipulaDados(operacao, controle) {
     }
 }
 
+const estatisticas = document.querySelectorAll("data-estatisticas").
+console.log(estatisticas)
 
+const estatistica = document.querySelectorAll("data-estatistica").
+console.log(estatistica)
 
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.datasete.controle, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.controle)
+    })
+})
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach( (elemento ) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+}
